347415e8538eef4033c961571044d68c90b56e44
VICKYECE2025 <vigneshece11@gmail.com>
2025-02-01 16:37:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
  # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "mybucket2026 "
+    bucket = "dev-bucket-25"
     key    = "pro/terraform.tfstate"
     region = "ap-south-1"   
   }
